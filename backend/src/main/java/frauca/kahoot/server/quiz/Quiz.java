060ca6e77cf350d7a18e7150fec744276ffe9d38
Roger Frauca <roger.frauca@marfeel.com>
2020-02-04 22:01:04 +0100

    git ignore to initialize git

---
+++ backend/src/main/java/frauca/kahoot/server/quiz/Quiz.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020 by Marfeel Solutions (http://www.marfeel.com)
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Marfeel Solutions S.L and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Marfeel Solutions S.L and its
+ * suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Marfeel Solutions SL.
+ */
+
+package frauca.kahoot.server.quiz;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Builder;
+import lombok.Value;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Value
+@Builder
+@Document
+@JsonDeserialize(builder = Quiz.QuizBuilder.class)
+class Quiz {
+
+    @JsonPOJOBuilder(withPrefix = "")
+    static class QuizBuilder {
+
+    }
+}
