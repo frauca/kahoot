c28e858017ccf2a83d261794a6aa9f63beb336d3
Roger Frauca <roger.frauca@marfeel.com>
2020-05-01 08:41:49 +0200

    no way to go directly to /register page.. as it does not exists

---
+++ frontend/pages/goto.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>You will be redirected to {{ uri }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Goto',
+  data() {
+    return {
+      uri: this.$route.query.uri
+    }
+  },
+  created() {
+    this.$router.push({
+      path: this.uri
+    })
+  }
+}
+</script>
+
+<style scoped></style>
